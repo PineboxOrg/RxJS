2d16812b8b2ec6c89ed03cd928b9a56417dc287b
Sebastian Velasquez Guevara <rlxsebas@gmail.com>
2018-10-18 12:41:53 -0500

    [ Lesson 1 ] - Create observable from events and using ajax to retrieve data

--- index.ts
+++ index.ts
@@ -1,43 +1,35 @@
 import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
+let button = document.getElementById('readersButton');
 /**
- * ## Create a observable using new keyword or create
- * new Observable() === Observable.create()
- */
-let allBooksObservable$ = Observable.create(subscriber => {
-    if (document.title !== 'RxBookTracker') {
-        subscriber.error('Incorrect page title');
-    }
-    for (let book of allBooks) {
-        subscriber.next(book);
-    }
-    setTimeout(() => {
-        subscriber.complete();
-    }, 2000);
-    return () => console.log('Clean up');
-});
+ * # Create observable using fromEvent
+ * Using static data
+*/
+fromEvent(button, 'click')
+// .subscribe((event) => {
+//     console.log(event);
 
-// allBooksObservable$.subscribe((book: any) => console.log(book.title))
+//     let readersDiv = document.getElementById('readers');
+//     for (let reader of allReaders) {
+//         readersDiv.innerHTML += `${reader.name} <br>`;
+//     }
+// })
 
 /**
- * ## Create observable using of
- */
-let source1$ = of('hello', 10, true, allReaders[0].name);
-
-// source1$.subscribe((value) => console.log(value));
-
-/**
- * ## Create observable using from
+ * # Create observable using fromEvent
+ * Getting data from API
  */
 
-let source2$ = from(allBooks);
-// source2$.subscribe((value) => console.log(value));
-
-
-/**
- * ## Combine observables using concat
- */
-
-concat(source1$, source2$)
-    .subscribe((value) => console.log(value));
+fromEvent(button, 'click')
+    .subscribe((event) => {
+        console.log(event);
+        ajax('/api/readers').subscribe((ajaxResponse) => {
+            console.log(ajaxResponse);
+            let readersDiv = document.getElementById('readers');
+            for (let reader of ajaxResponse.response) {
+                readersDiv.innerHTML += `${reader.name} <br>`;
+            }
+        })
+    });
